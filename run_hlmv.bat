40ade8ecb0492d7a5bf621370397bdd1d0753ce7
UAVXP <uavxp29@gmail.com>
2013-04-30 23:06:20 +0300

    First commit

---
+++ run_hlmv.bat
@@ -0,0 +1,2 @@
+@"d:\steam\steamapps\uavxp95\sourcesdk\bin\source2007\bin\hlmv.exe" -game "d:\steam\steamapps\SourceMods\corrmp" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
---
+++ run_studiomdl.bat
@@ -0,0 +1,2 @@
+@"d:\steam\steamapps\uavxp95\sourcesdk\bin\source2007\bin\studiomdl.exe" -game "d:\steam\steamapps\SourceMods\corrmp" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
